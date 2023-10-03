d8bb5a3a704ebf1fc7b2323a26a36ac1ebee4d62
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-10-03 18:08:41 +0200

    Added header section with css files

--- rimty-photo/src/App.js
+++ rimty-photo/src/App.js
@@ -1,12 +1,11 @@
-
 import './App.css';
-
+import Header from "./components/Header/Header";
+import Cover from "./components/Cover/Cover";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Cover/>
+     <Header/>
     </div>
   );
 }
---
+++ rimty-photo/src/components/Cover/Cover.jsx
@@ -0,0 +1,17 @@
+import "./Cover.css";
+
+function Cover() {
+  return (
+    <section className="cover">
+      {/* <div className="overlay">
+        <h1>Overlay</h1>
+      </div>
+      <video className="cover-img" src="videos/banana.mp4" />
+      <div className="content">
+        <h1>Fotographie</h1>
+        <p>Photography website by Rimty</p>
+      </div> */}
+    </section>
+  );
+}
+export default Cover;

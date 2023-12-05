45d95bbe67807b2fec7c60100d33ff8d711c2994
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-12-05 23:20:18 +0100

    composed page

--- rimty-photo/src/components/Compose/Compose.jsx
+++ rimty-photo/src/components/Compose/Compose.jsx
@@ -2,11 +2,69 @@ import "./Compose.css";
 function Compose() {
   return (
     <>
-      <section id="composeBlog">
-        <div className="compose container col-xxl-8 px-4 py-5">
-          <div className="composeImg">
-            <img src="./images/kashful.jpg" alt="" />
-          </div>
+      <section id="newblog">
+        <div className="container comp-container col-xxl-8 px-4 py-4">
+          <img
+            className="col-xxl-8 px-4 py-4 composeImg animate__animated animate__zoomInDown  animate__delay-1s"
+            alt="comp-img"
+            src="images/kashful.jpg"
+          />
+
+          <form className="composeForm">
+            <div className="composeFormGroup flexCenter">
+              <label>Cover Photo</label>
+              <input
+                id="fileInput"
+                accept="image/*"
+                className="composeBlog"
+                type="file"
+                autoFocus={true}
+              />
+            </div>
+            <div className="composeFormGroup">
+              <label>Title</label>
+              <input
+                className="composeBlog"
+                type="text"
+                placeholder="Title"
+                autoFocus={true}
+              />
+            </div>
+            <div className="composeFormGroup">
+              <label>Time</label>
+              <input
+                className="composeBlog"
+                type="text"
+                placeholder="Time"
+                autoFocus={true}
+              />
+            </div>
+
+            <div className="composeFormGroup">
+              <label>Qoute</label>
+              <input
+                className="composeBlog"
+                type="text"
+                placeholder="Qoute"
+                autoFocus={true}
+              />
+            </div>
+
+            <div className="composeFormGroup" id="composeEdit">
+              <label>Description</label>
+              <textarea
+                className="composeBlog"
+                type="text"
+                placeholder="Write a blog..."
+                cols="30"
+                rows="10"
+              ></textarea>
+            </div>
+
+            <button className="composeBlogBtn" type="submit" autoComplete="off">
+              Publish
+            </button>
+          </form>
         </div>
       </section>
     </>

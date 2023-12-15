76207840b33992d937fcdb21edec1c31b9930463
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-12-15 20:49:39 +0100

    create routes in blogs.js

--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -3,7 +3,11 @@ import { addBlog } from "../controllers/blog.js";
 
 const router = express.Router()
 
-
+router.get("/",) //get all post
+router.get("/:id",) //get single post
+router.post("/",)//new post
+router.delete("/:id",)//delete post
+router.update("/:id",)//update post
 
 
 export default router;

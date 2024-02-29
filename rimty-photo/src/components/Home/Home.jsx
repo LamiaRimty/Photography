d0cdf391c4c4fbb3292fb7079c5c99d53763cbbc
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-02-29 20:55:44 +0100

    changed bio

--- rimty-photo/src/components/Home/Home.jsx
+++ rimty-photo/src/components/Home/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./Home.css";
 import Gallery from "../Gallery/Gallery";
 import Blog from "../Blog/Blog";
@@ -23,6 +24,62 @@ function Home() {
 
   return (
     <>
+      <section id="home">
+        <div className="container col-xxl-8 px-4 py-4">
+          <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div className="col-10 col-sm-8 col-lg-6  home-img animate__animated animate__zoomInDown  animate__delay-1s">
+              <img
+                src="images/BikerGirl.png"
+                // src="https://i.etsystatic.com/8538461/r/il/21a140/1038308502/il_300x300.1038308502_j77d.jpg"
+                class="d-block  mx-lg-auto img-fluid"
+                alt="biker girl"
+                width="500"
+                height="500"
+                loading="lazy"
+              />
+            </div>
+            {/* <div class=" col-10 col-sm-8 col-lg-6 justify-content-center home-img animate__animated animate__slideInLeft animate__delay-1s">
+            <img
+              id="rimty"
+              src="./images/Rimty.jpg"
+              class="about-img d-block mx-lg-auto img-fluid"
+              alt="Rimty"
+              loading="lazy"
+            />
+          </div> */}
+            <div className="col-lg-6">
+              {/* <h3 className="home-subtitle animate__animated animate__backInRight">
+              Hello
+            </h3> */}
+              <h1 className="display-5 fw-bold lh-1 mb-3 home-title animate__animated animate__rollIn">
+                Hi,I'm <span ref={el} className="home-typed" />
+              </h1>
+
+              <p className="home-desc animate__animated animate__backInRight">
+                Travel Photography Blog by{" "}
+                <a
+                  className="home-desc"
+                  href="https://www.linkedin.com/in/lamia-jabin-rimty-0020a2219/"
+                >
+                  Lamia Jabin Rimty
+                </a>
+              </p>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                <a
+                  href="#contact"
+                  type="button"
+                  // class="btn btn-outline-secondary btn-lg px-4 button button-flex home-button"
+                  className="button btn-lg px-4 me-md-2 animate__animated animate__backInUp animate__delay-1s"
+                >
+                  Contact Me
+                  <i className="uil uil-message button-icon"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="b-example-divider"></div>
+      </section>
       {/* <section className="home">
         <div className="container container-home col-xxl-8 px-4 py-4">
           <div className="home-title">
